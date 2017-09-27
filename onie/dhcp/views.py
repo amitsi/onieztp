58198e9bc7cf07af23c647b4f646e41023134950
Gaurav Bajaj <gaurav.bajaj@calsoftinc.com>
2017-09-27 09:52:21 +0530

    hosts file follows gui pattern and tablurised display

--- onie/dhcp/views.py
+++ onie/dhcp/views.py
@@ -6,6 +6,8 @@ from .forms import DhcpForm
 from dhcp.models import Post
 
 # Create your views here.
+dhcp_config = "/tmp/dhcpd.conf"
+hosts_config = "/tmp/hosts"
 
 
 def dhcp_switch_add(request):
@@ -13,9 +15,10 @@ def dhcp_switch_add(request):
     :param request: It checks whether the request is 'POST' or not
     :return: Render the dhcp form as the return value
     """
+    global dhcp_config
     display = Post.objects.all()
     if request.method == "POST":
-        print request.POST.getlist('switch')
+        #print request.POST.getlist('switch')
         form = DhcpForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['switch_name']
@@ -24,7 +27,7 @@ def dhcp_switch_add(request):
             default_url = form.cleaned_data['default_url']
             form.save()
 
-            dhcp_file = open("/tmp/dhcpd.conf", "a")
+            dhcp_file = open(dhcp_config, "a")
             str_onie = "\nhost " + name
             str_onie += "{\nhardware ethernet " + ip_addr
             str_onie += ";\nfixed-address " + mac_addr
@@ -42,17 +45,19 @@ def dhcp_switch_add(request):
 
 
 def make_host_file(request):
+    global hosts_config
     if request.method == "POST":
         switch = request.POST.getlist('switch')
         section = request.POST.getlist('section_name')[0]
-        print section
-        hosts_file = open("/tmp/hosts", "a")
+        #print section
+        hosts_file = open(hosts_config, "a")
         hosts_file.write("[" + section + "]\n")
-        print switch
+        #print switch
         for switch_name in switch:
             p = Post.objects.get(switch_name=switch_name)
-            str_hosts = switch_name + " ansible_host=" + p.ip_addr + '\n'
-            print str_hosts
+            str_hosts = switch_name + " ansible_host=" + p.ip_addr
+            str_hosts += ' ansible_user="{{ SSH_USER }}" ansible_ssh_pass="{{ SSH_PASS }}"\n'
+            #print str_hosts
             hosts_file.write(str_hosts)
         hosts_file.write("\n")
         hosts_file.close()
